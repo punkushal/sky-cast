c64f8ab89193e2cd600073bee81cf887c65c5ab1
punkushal <spycymagar@gmail.com>
2025-12-06 21:32:28 +0545

    weather model created for current weather and hourly forecast

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,137 @@
+// weather model for current data
+class CurrentWeather {
+  List<Weather> weather;
+  Main main;
+  int visibility;
+  Wind wind;
+  Sys sys;
+  String name;
+
+  CurrentWeather({
+    required this.weather,
+    required this.main,
+    required this.visibility,
+    required this.wind,
+    required this.sys,
+    required this.name,
+  });
+
+  // convert json to dart object
+  factory CurrentWeather.fromJson(Map<String, dynamic> json) {
+    return CurrentWeather(
+      weather: (json['weather'] as List)
+          .map((item) => Weather.fromJson(item))
+          .toList(),
+      main: Main.fromJson(json),
+      visibility: json['visibility'],
+      wind: Wind.fromJson(json),
+      sys: Sys.fromJson(json),
+      name: json['name'],
+    );
+  }
+}
+
+class Main {
+  double temp;
+  double feelsLike;
+  int pressure;
+  int humidity;
+
+  Main({
+    required this.temp,
+    required this.feelsLike,
+    required this.pressure,
+    required this.humidity,
+  });
+
+  factory Main.fromJson(Map<String, dynamic> json) {
+    return Main(
+      temp: json['temp'],
+      feelsLike: json['feelsLike'],
+      pressure: json['pressure'],
+      humidity: json['humidity'],
+    );
+  }
+}
+
+class Sys {
+  String country;
+  int sunrise;
+  int sunset;
+
+  Sys({required this.country, required this.sunrise, required this.sunset});
+
+  factory Sys.fromJson(Map<String, dynamic> json) {
+    return Sys(
+      country: json['country'],
+      sunrise: json['sunrise'],
+      sunset: json['sunset'],
+    );
+  }
+}
+
+class Weather {
+  String main;
+  String description;
+
+  Weather({required this.main, required this.description});
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(main: json['main'], description: json['description']);
+  }
+}
+
+class Wind {
+  double speed;
+
+  Wind({required this.speed});
+
+  factory Wind.fromJson(Map<String, dynamic> json) =>
+      Wind(speed: json['speed']);
+}
+
+// hourly forecast model
+class Hourlyforecast {
+  List<ListElement> list;
+
+  Hourlyforecast({required this.list});
+
+  factory Hourlyforecast.fromJson(Map<String, dynamic> json) {
+    return Hourlyforecast(
+      list: (json['list'] as List)
+          .map((item) => ListElement.fromJson(json))
+          .toList(),
+    );
+  }
+}
+
+class ListElement {
+  Main main;
+  List<Weather> weather;
+  Wind wind;
+  int visibility;
+
+  // forecasted date time
+  String dtTxt;
+
+  ListElement({
+    required this.main,
+    required this.weather,
+    required this.wind,
+    required this.visibility,
+    required this.dtTxt,
+  });
+
+  // convert json to dart object
+  factory ListElement.fromJson(Map<String, dynamic> json) {
+    return ListElement(
+      main: Main.fromJson(json),
+      weather: (json['weather'] as List)
+          .map((item) => Weather.fromJson(item))
+          .toList(),
+      wind: Wind.fromJson(json),
+      visibility: json['visibility'],
+      dtTxt: json['dtTxt'],
+    );
+  }
+}
