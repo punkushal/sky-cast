b2a794dd191bcb0feb7c32de983195dbc291074b
punkushal <spycymagar@gmail.com>
2025-12-09 21:18:20 +0545

    env file loaded and loading page added as home

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 import 'package:skycast/constants/theme/app_theme.dart';
+import 'package:skycast/pages/loading_page.dart';
+import 'package:skycast/providers/weather_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await dotenv.load(fileName: '.env');
+  } catch (e) {
+    throw Exception('Api key not avaialble');
+  }
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => WeatherProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +28,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: Scaffold(body: Center(child: TextField())),
+      home: LoadingPage(),
     );
   }
 }
